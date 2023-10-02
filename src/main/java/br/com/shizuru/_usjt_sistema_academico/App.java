9c874bf205e0e63104f5afa28e1fbc3ad163e416
Lucca Shizuru <luccashizuru@gmail.com>
2023-10-02 20:57:24 -0300

    ap11(login): campos login e senha

---
+++ src/main/java/br/com/shizuru/_usjt_sistema_academico/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.shizuru._usjt_sistema_academico;
+
+/**
+ *
+ * @author 823117825
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
